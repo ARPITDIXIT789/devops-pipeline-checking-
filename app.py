52525d92904cb540987d75087bdee7d623863a95
ARPIT DIXIT <arpitdixit942@gmail.com>
2025-12-19 11:55:10 +0530

    Remove brackets from name in home route message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 DevOps CI/CD Pipeline Deployed Successfully by [ARPIT DIXIT] on AWS EC2!"
+    return "🚀 DevOps CI/CD Pipeline Deployed Successfully by ARPIT DIXIT on AWS EC2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
